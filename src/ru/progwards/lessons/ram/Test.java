66b8949eccb6f9a524766cab294885e031950aa2
Twist328 <evgen-shevel@yandex.ru>
2021-04-20 21:43:27 +0300

    ссс

--- src/ru/progwards/lessons/ram/Test.java
+++ src/ru/progwards/lessons/ram/Test.java
@@ -1,14 +1,11 @@
 package ru.progwards.lessons.ram;
-
-
-
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Calculator c = new Calculator();
-        System.out.print(c.sum(10,10));
+        int a= 10;int b=15;
+        System.out.println(c.sum(a,b));
     }
 }
-
 /*
 C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\javac" -sourcepath src -d out src/ru/progwards/java2/lessons/ram/Test.java
 C:\Projects\java2>java -classpath .\out ru.progwards.java2.lessons.ram.Test
