998dcac41f6da6068e77bd86152c2bcd7a9fcd89
Twist <evgen-shevel@yandex.ru>
2021-04-20 11:08:14 +0300

    bbb

---
+++ src/ru/progwards/lessons/HelloWorld.java
@@ -0,0 +1,19 @@
+package ru.progwards.lessons;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java ! This is console JAVA HelloWrorld.");
+    }
+}
+
+/*
+C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\javac" -d out src/ru/progwards/java2/lessons/ram/HelloWorld.java
+C:\Projects\java2>java -classpath .\out ru.progwards.java2.lessons.ram.HelloWorld
+*/
+
+/*
+C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\javac" -d out src/ru/progwards/java2/lessons/ram/HelloWorld.java
+C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\jar" cfe hello.jar HelloWorld out/ru/progwards/java2/lessons/ram/HelloWorld.class
+C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\java" -jar hello.jar
+Error: Could not find or load main class HelloWorld              -- ??
+*/
---
+++ src/ru/progwards/lessons/Ram.java
@@ -0,0 +1,13 @@
+package ru.progwards.lessons;
+
+public class Ram {
+}
+     class Calculator {
+
+        public static void main(String[] args) { }
+        public int sum(int a, int b) {
+            a=4;
+            return a+b;
+        }
+    }
+
