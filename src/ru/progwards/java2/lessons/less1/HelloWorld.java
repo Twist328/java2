dcf0a861bd492f74ede6d287917cb7c9f0e721f0
Twist <evgen-shevel@yandex.ru>
2021-04-21 08:52:21 +0300

    bbb

--- src/ru/progwards/java2/lessons/less1/HelloWorld.java
+++ src/ru/progwards/java2/lessons/less1/HelloWorld.java
@@ -12,8 +12,8 @@ C:\Projects\java2>java -classpath .\out ru.progwards.java2.lessons.ram.HelloWorl
 */
 
 /*
-C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\javac" -d out src/ru/progwards/java2/lessons/ram/HelloWorld.java
-C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\jar" cfe hello.jar HelloWorld out/ru/progwards/java2/lessons/ram/HelloWorld.class
-C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\java" -jar hello.jar
+C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\javac" -d out src/ru/progwards/java2/lessons/less1/HelloWorld.java
+C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\jar" cfe hello.jar HelloWorld out/ru/progwards/java2/lessons/less1/HelloWorld.
+classC:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\java" -jar hello.jar
 Error: Could not find or load main class HelloWorld              -- ??
 */
