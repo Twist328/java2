8dc58e4a4bc40a867f1e38092381426e7de1acef
Twist <evgen-shevel@yandex.ru>
2021-04-23 16:06:51 +0300

    bbb

--- src/ru/progwards/java2/lessons/less1/Test.java
+++ src/ru/progwards/java2/lessons/less1/Test.java
@@ -1,12 +1,26 @@
 package ru.progwards.java2.lessons.less1;
 
 public class Test {
-    public static void main(String... args) {
-        Calculator c = new Calculator();
-        int a= 10;int b=15;
-        System.out.println(c.sum(a,b));
+    static int sumSequence(int n) {
+        if (n == 1)
+            return n;
+        return sumSequence(n - 2) + n;
     }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+        System.out.println(c.sum(10, 15));
+        //System.out.println(sumSequence(2));
+        System.out.println(sumSequence(1));
+        System.out.println(sumSequence(3));
+    }
+
 }
+
+
+
+
 /*
 C:\Projects\java2>"C:\Program Files\Java\jdk14.0.1\bin\javac" -sourcepath src -d out src/ru/progwards/java2/lessons/ram/Test.java
 C:\Projects\java2>java -classpath .\out ru.progwards.java2.lessons.ram.Test
