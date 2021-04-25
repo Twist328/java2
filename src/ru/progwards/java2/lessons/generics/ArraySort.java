0d9af55479a67a85a0e427bde0a506b70149d60f
Twist328 <evgen-shevel@yandex.ru>
2021-04-25 07:43:15 +0300

    bbb

--- src/ru/progwards/java2/lessons/generics/ArraySort.java
+++ src/ru/progwards/java2/lessons/generics/ArraySort.java
@@ -27,7 +27,7 @@ public class ArraySort {
 
         for (int i = array.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (array[j].compareTo(array[i]) > 0) {
+                if (array[j].compareTo(array[i]) == 1) {
                     T tmpValue = array[i];
                     array[i] = array[j];
                     array[j] = tmpValue;
