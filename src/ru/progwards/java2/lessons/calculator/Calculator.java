2daf516a09e0c1c5acd7f88f54eac23f65f6261d
Twist328 <evgen-shevel@yandex.ru>
2021-06-02 08:49:07 +0300

    nnn

--- src/ru/progwards/java2/lessons/calculator/Calculator.java
+++ src/ru/progwards/java2/lessons/calculator/Calculator.java
@@ -5,21 +5,28 @@ package ru.progwards.java2.lessons.calculator;
 //import java.util.regex.Pattern;
 
 public class Calculator {
+    private int     pos;        // в каком месте строки разбираем
+    private int     len;
 
-    //Реализуйте метод public static int calculate(String expression), который вычисляет арифметическое выражение,
-    // заданное в виде символьной строки. Выражение содержит только целые цифры и знаки арифметических операций +-*/
-    //При вычислении должны учитываться приоритеты операций, например, результат вычисления выражения "2+3*2"
-    //должен быть равен 8. По оригинальному условию задачи все числа содержат не более одной цифры, пробелов в строке нет.
+    public Calculator() {
 
-    public static int calculate(String expression) {
+        /*Реализуйте метод public static int calculate(String expression), который вычисляет арифметическое выражение,
+         заданное в виде символьной строки. Выражение содержит только целые цифры и знаки арифметических операций +-
+         При вычислении должны учитываться приоритеты операций, например, результат вычисления выражения "2+3*2"
+         должен быть равен 8. По оригинальному условию задачи все числа содержат не более одной цифры, пробелов в строке нет.*/
+    }
+    public static int calculate(String expression) throws  NumberFormatException{
         String str = expression;
         int i = Integer.valueOf(str.substring(0, 1));
         if (str.length() == 1) return i;
         String s = str.substring(1, 2);
+
         while (s.equals("*") || s.equals("/")) {
             if (s.equals("*")) {
+
                 i *= Integer.valueOf(str.substring(2, 3));
-            }else if (s.equals("/"))
+
+            } else if (s.equals("/"))
                 i /= Integer.valueOf(str.substring(2, 3));
             str = str.substring(2);
             if (str.length() == 1) return i;
@@ -27,14 +34,16 @@ public class Calculator {
         }
         if (s.equals("+")) {
             i += calculate(str.substring(2));
-        }else if (s.equals("-"))
+        } else if (s.equals("-"))
             i -= calculate(str.substring(2));
         return i;
+
     }
 
     public static void main(String[] args) {
-        System.out.println(calculate("2+3*2+8/2")); //12
-        System.out.println(calculate("1+1*2-5/5")); //2
+        System.out.println(calculate("3/3*2+8/2")); //6
+        System.out.println(calculate("1*1*2-9/3-9")); //8
+        //System.out.println(calculate("(4-2)/1+(1*9)"));
 
     }
 }
