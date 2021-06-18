616ce3debc608dfa6837c74fc33144a8a2ca141b
Twist328 <evgen-shevel@yandex.ru>
2021-06-18 20:00:21 +0300

    nnn

---
+++ src/ru/progwards/java2/lessons/annotation/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Пакет для исследования Javadoc.
+ * Содержит  класс AvlTree - АВЛ дерево, с методами:
+ * 2.1 public void put(K key, V value) - с добавлением пары ключ-значение, или если уже такой ключ есть - его замена
+ * 2.2 public void delete(K key) - удаление ключа
+ * 2.3 public V find(K key) - поиск ключа
+ * 2.4 public void change(K oldKey, K newKey) - замена значения ключа на другое
+ * 2.5 public void process(Consumer<TreeLeaf<K,V>> consumer) - прямой обход дерева
+ * Также имеются другие вспомогательные методы и переменные, а также тест
+ */
+package ru.progwards.java2.lessons.annotation;
