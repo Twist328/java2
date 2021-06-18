86e5ac5e86a5a5edde5df93e7c3885aa597381ef
Twist328 <evgen-shevel@yandex.ru>
2021-06-18 22:51:34 +0300

    javadoc

--- src/ru/progwards/java2/lessons/annotation/TreeException.java
+++ src/ru/progwards/java2/lessons/annotation/TreeException.java
@@ -3,7 +3,6 @@ package ru.progwards.java2.lessons.annotation;
 /**
  * Класс исключение
  */
-
 public class TreeException extends Exception {
     public TreeException(String message) {
         super(message);
