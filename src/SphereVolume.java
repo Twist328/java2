d806f5662f44ce83dd2e5b7f4ba0932dd781db1a
Twist328 <evgen-shevel@yandex.ru>
2021-06-22 21:42:27 +0300

    aa

--- src/SphereVolume.java
+++ src/SphereVolume.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SphereVolume {  // объем шара
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите радиус шара:");
+        System.out.println("\n" + "Введите радиус шара:");
         double radius = sc.nextDouble();
         double volume = (4 * 3.14 * Math.pow(radius, 3)) / 3;// формула V= (4* PI (3,14)) * (rad * rad * rad)) /3;
         System.out.println("Объем шара = " + volume);
