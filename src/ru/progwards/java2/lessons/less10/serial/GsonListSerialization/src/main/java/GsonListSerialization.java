a00cc228a861f7d5a2e18c978d2eddceab1ab6fa
Twist <evgen-shevel@yandex.ru>
2021-06-23 12:17:48 +0300

    тт

--- src/ru/progwards/java2/lessons/less10/serial/GsonListSerialization/src/main/java/GsonListSerialization.java
+++ src/ru/progwards/java2/lessons/less10/serial/GsonListSerialization/src/main/java/GsonListSerialization.java
@@ -2,15 +2,11 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.Serializable;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
-
-import static com.sun.tools.attach.VirtualMachine.list;
 
 public class GsonListSerialization {
 
