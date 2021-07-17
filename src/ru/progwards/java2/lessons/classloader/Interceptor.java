d6de658e06ab5be3619f3d515ca28ed1e2884062
Twist328 <evgen-shevel@yandex.ru>
2021-07-17 16:14:32 +0300

    aa

--- src/ru/progwards/java2/lessons/classloader/Interceptor.java
+++ src/ru/progwards/java2/lessons/classloader/Interceptor.java
@@ -1,9 +1,10 @@
 package ru.progwards.java2.lessons.classloader;
 
+import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 
 public class Interceptor {
     public static void premain(String agentArgument, Instrumentation instrumentation) {
-        instrumentation.addTransformer(new SystemProfiler(agentArgument));
+        instrumentation.addTransformer((ClassFileTransformer) new SystemProfiler());
     }
 }
