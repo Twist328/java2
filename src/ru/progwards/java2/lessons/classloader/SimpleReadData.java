c6b7c1784dd513319852c3f627854e036ac155dc
Twist <evgen-shevel@yandex.ru>
2021-07-19 08:59:35 +0300

    тт

--- src/ru/progwards/java2/lessons/classloader/SimpleReadData.java
+++ src/ru/progwards/java2/lessons/classloader/SimpleReadData.java
@@ -5,18 +5,21 @@ public class SimpleReadData {
     final String name;
 
     public SimpleReadData() {
-        //try { Thread.sleep(5); } catch (InterruptedException e) { e.printStackTrace(); }
+        try { Thread.sleep(5); } catch (InterruptedException e) { e.printStackTrace(); }
         name = "SRD"+String.valueOf(System.currentTimeMillis());
     }
 
     public String toString() {
-        //try { Thread.sleep(2); } catch (InterruptedException e) { e.printStackTrace(); }
+        try { Thread.sleep(2); } catch (InterruptedException e) { e.printStackTrace(); }
         return name;
     }
 
     public static void main(String[] args) {
-        //try { Thread.sleep(27); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        try { Thread.sleep(27); } catch (InterruptedException e) { e.printStackTrace(); }
+        System.out.println("\n******************************");
         System.out.println(new SimpleReadData());
+        System.out.println("******************************");
     }
 
 }
