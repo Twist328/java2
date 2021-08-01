769004d896b945966d2d8831c049a2f13cb76e4c
Twist328 <evgen-shevel@yandex.ru>
2021-08-01 18:53:06 +0300

    bb

--- src/ru/progwards/java2/lessons/calculator/Calculator.java
+++ src/ru/progwards/java2/lessons/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package ru.progwards.java2.lessons.calculator;
 
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Calculator {
 
     public static void main(String[] args) {
@@ -183,9 +186,10 @@ class Calculator3 {// Тренировка за 40 мин
 
     public static void main(String[] args) {
 
-        System.out.println("***********************************");
-        System.out.println("результат вычисления:    " + calculate3("8/2*(2+2)*9"));//144
-        System.out.println("***********************************");
+        System.out.println("\n***********************************"+ Emoji.SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES);
+        System.out.println("результат вычисления: " + calculate3("8/2*(2+2)*9")+(LocalDateTime.now().format(DateTimeFormatter.ofPattern
+                (" СЕГОДНЯ   " + "dd-MM-YYYY ")) + (LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm ")))));//144
+        System.out.println("***********************************"+ Emoji.SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES);
     }
 
     public Calculator3(String exspression) {
@@ -299,5 +303,42 @@ class Calculator3 {// Тренировка за 40 мин
                 return result;
         }
         return result;
+    }
+    enum Emoji {
+
+        GRINNING_FACE_WITH_SMILING_EYES('\uD83D', '\uDE01'),
+        FACE_WITH_TEARS_OF_JOY('\uD83D', '\uDE02'),
+        SMILING_FACE_WITH_OPEN_MOUTH('\uD83D', '\uDE03'),
+        SMILING_FACE_WITH_OPEN_MOUTH_AND_SMILING_EYES('\uD83D', '\uDE04'),
+        SMILING_FACE_WITH_OPEN_MOUTH_AND_COLD_SWEAT('\uD83D', '\uDE05'),
+        SMILING_FACE_WITH_OPEN_MOUTH_AND_TIGHTLY_CLOSED_EYES('\uD83D', '\uDE06'),
+        WINKING_FACE('\uD83D', '\uDE09');
+
+        Character firstChar;
+        Character secondChar;
+
+        Emoji(Character firstChar, Character secondChar) {
+            this.firstChar = firstChar;
+            this.secondChar = secondChar;
+        }
+
+        Emoji() {
+
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            if (this.firstChar != null) {
+                sb.append(this.firstChar);
+            }
+            if (this.secondChar != null) {
+                sb.append(this.secondChar);
+            }
+
+            return sb.toString();
+        }
+
     }
 }
