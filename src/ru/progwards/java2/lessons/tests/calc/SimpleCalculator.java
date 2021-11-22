e687d3633fce169c6c3b2335153f7e6ef27342ea
Twist328 <evgen-shevel@yandex.ru>
2021-11-22 19:02:51 +0300

    ..

--- src/ru/progwards/java2/lessons/tests/calc/SimpleCalculator.java
+++ src/ru/progwards/java2/lessons/tests/calc/SimpleCalculator.java
@@ -1,26 +1,28 @@
 package ru.progwards.java2.lessons.tests.calc;
 
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import ru.progwards.java2.lessons.tests.test.calc.IntOverflowExc;
 
 public class SimpleCalculator {
 
     public static int sum(int val1, int val2) throws IntOverflowExc {
-        long longRes = ((long)val1) + val2;
+        long longRes = ((long) val1) + val2;
         if (longRes > Integer.MAX_VALUE || longRes < Integer.MIN_VALUE)
             throw new IntOverflowExc("Int range overflow during addition " + longRes);
         return val1 + val2;
     }
 
     public static int diff(int val1, int val2) throws IntOverflowExc {
-        long longRes = ((long)val1) - val2;
+        long longRes = ((long) val1) - val2;
         if (longRes > Integer.MAX_VALUE || longRes < Integer.MIN_VALUE)
             throw new IntOverflowExc("Int range overflow during on subtraction " + longRes);
         return val1 - val2;
     }
 
     public static int mult(int val1, int val2) {
-        long longRes = (long)val1 * val2;
+        long longRes = (long) val1 * val2;
         if (longRes > Integer.MAX_VALUE || longRes < Integer.MIN_VALUE)
             throw new IntOverflowExc("Int range overflow during on mult " + longRes);
         return val1 * val2;
@@ -29,8 +31,20 @@ public class SimpleCalculator {
     public int div(int val1, int val2) {
         if (val2 == 0) throw new ArithmeticException("cannot be divided by zero");
         long longRes = (long) val1 / val2;
-        if (longRes > Integer.MAX_VALUE || longRes < Integer.MIN_VALUE) throw new ArithmeticException("integers is over");
+        if (longRes > Integer.MAX_VALUE || longRes < Integer.MIN_VALUE)
+            throw new ArithmeticException("integers is over");
         return val1 / val2;
     }
-}
 
+    @RunWith(Suite.class)
+    @Suite.SuiteClasses({
+            SimpleCalculatorDiffTest.class,
+            SimpleCalculatorDivTest.class,
+            SimpleCalculatorMultTest.class,
+            SimpleCalculatorSumTest.class
+    })
+
+
+    public class SimpleCalculatorAllTest {
+    }
+}
---
+++ src/ru/progwards/java2/lessons/tests/calc/SimpleCalculatorMultTest.java
@@ -0,0 +1,4 @@
+package ru.progwards.java2.lessons.tests.calc;
+
+public class SimpleCalculatorMultTest {
+}
